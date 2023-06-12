ce36ca26bb1dc97d30eebc1ada49c8c3bde48837
Konatham komal eshwara kumar <komaleshwarakumarkonatham@gmail.com>
2023-06-12 07:33:23 +0530

    SDFS

--- sql_query/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "mbts";
-
-$conn =     mysqli_connect($hostname,$username,$password,$database);
-session_start();
-?>
